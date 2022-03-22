1b0548f5267d2d3e72c02f08f7defc822f548bb0
Paulo Magno <pgs.magno@gmail.com>
2022-03-22 17:38:54 -0300

    adds cards container logic, links, back buttons, transtions

--- app.js
+++ app.js
@@ -297,10 +297,220 @@ function main() {
     "restBarCafe",
     "transpTur"]
 
+    const elemH1 = document.querySelector("h1");
+    const backgroundDiv = document.querySelector(".background");
 
+    const backBtn = document.createElement("img");
+    backBtn.src = "./images/back.png"
+    backBtn.classList.add("back-btn");
+
+    function generateCardsFromIndicador(indicador, titulo) {
+
+        
+        backgroundDiv.insertBefore(backBtn, elemH1);
+
+        elemH1.textContent = titulo;
+
+        toggleSelectorAndIndicadores();
+
+        const entriesContainer = document.createElement("div");
+        
+
+        entriesContainer.classList.add("entries-container");
+
+        if (indicador == "guiaPF") {
+            for(let i = 0; i < eval(indicador).length; i++){
+            let entryDiv = document.createElement("div");
+            let entryTitle = document.createElement("h4");
+            let entryNumber = document.createElement("span");
+            let entryEmail = document.createElement("span");
+            let entryWebsite = document.createElement("span");
+
+            entryTitle.textContent = eval(indicador)[i]["Nome Completo"];
+            entryNumber.textContent = `Telefone: ${eval(indicador)[i]["Telefone"]};`
+            entryEmail.textContent = `Email: ${eval(indicador)[i]["E-mail Alternativo/Comercial"]}`;
+            entryWebsite.textContent = `Website: ${eval(indicador)[i]["Website"]}`;
+
+            entryDiv.appendChild(entryTitle);
+            entryDiv.appendChild(entryNumber);
+            entryDiv.appendChild(entryEmail);
+            entryDiv.appendChild(entryWebsite);
+
+            entryDiv.classList.add("card");
+            entryDiv.classList.add("flex-item");
+
+            entriesContainer.appendChild(entryDiv);
+        }
+        } else {
+
+            for(let i = 0; i < eval(indicador).length; i++){
+
+                let entryDiv = document.createElement("div");
+                let entryTitle = document.createElement("h4");
+                let entryNumber = document.createElement("span");
+                let entryAddress = document.createElement("span");
+                let entryEmail = document.createElement("span");
+                let entryWebsite = document.createElement("span");
+
+                entryTitle.textContent = eval(indicador)[i]["Nome Fantasia"];
+                entryNumber.textContent = `Telefone: ${eval(indicador)[i]["Telefone Comercial"]};`
+                entryAddress.textContent = `Endereço: ${eval(indicador)[i]["Endereço Completo Comercial"]}`;
+                entryEmail.textContent = `Email: ${eval(indicador)[i]["E-mail Comercial"]}`;
+                entryWebsite.textContent = `Website: ${eval(indicador)[i]["Website"]}`;
+
+
+                entryDiv.appendChild(entryTitle);
+                entryDiv.appendChild(entryAddress);
+                entryDiv.appendChild(entryNumber);
+                entryDiv.appendChild(entryEmail);
+                entryDiv.appendChild(entryWebsite);
+
+                entryDiv.classList.add("card");
+                entryDiv.classList.add("flex-item");
+
+                entriesContainer.appendChild(entryDiv);
+                
+            }
+        }
+        backgroundDiv.appendChild(entriesContainer);
+
+
+    }
+    // const body = document.querySelector("body");
+    // body.addEventListener("click", toggleSelectorAndIndicadores);
+
+
+    function removeEntries () {
+        const entriesContainer = document.querySelector(".entries-container")
+        backgroundDiv.removeChild(entriesContainer);
+        backgroundDiv.removeChild(backBtn);
+    }
+
+    backBtn.addEventListener("click", () => {
+        elemH1.textContent = "Indicadores Turismo MA"
+        removeEntries();
+        toggleSelectorAndIndicadores();
+    })
+
+
+    const agenciasCard = document.querySelector(".card-agencias");
+    const acampamentosCard = document.querySelector(".card-acampamentos");
+    const casasEspCard = document.querySelector(".card-casasEsp");
+    const centroConvenCard = document.querySelector(".card-centroConven");
+    const empreenApoioCard = document.querySelector(".card-empreenApoio");
+    const empreenEntreteniCard = document.querySelector(".card-empreenEntreteni");
+    const guiaMEICard = document.querySelector(".card-guiaMEI");
+    const guiaPFCard = document.querySelector(".card-guiaPF");
+    const locadoraVeiculoCard = document.querySelector(".card-locadoraVeiculo");
+    const meioHospCard = document.querySelector(".card-meioHosp");
+    const organiEventCard = document.querySelector(".card-organiEvent");
+    const parqueTemaCard = document.querySelector(".card-parqueTema");
+    const prestEspCard = document.querySelector(".card-prestEsp");
+    const prestServInfraCard = document.querySelector(".card-prestServInfra");
+    const restBarCafeCard = document.querySelector(".card-restBarCafe");
+    const transpTurCard = document.querySelector(".card-transpTur");
+
+
+    agenciasCard.addEventListener("click", () => {
+        let titulo = "Agências";
+        generateCardsFromIndicador(indicadores[0], titulo);
+    });
+
+    acampamentosCard.addEventListener("click", () => {
+        let titulo = "Acampamentos";
+        generateCardsFromIndicador(indicadores[1], titulo);
+    });
+    
+    casasEspCard.addEventListener("click", () => {
+        let titulo = "Casas de Espetáculo";
+        generateCardsFromIndicador(indicadores[2], titulo);
+    });
+    
+    centroConvenCard.addEventListener("click", () => {
+        let titulo = "Centros de Convenções";
+        generateCardsFromIndicador(indicadores[3], titulo);
+    });
+    
+    empreenApoioCard.addEventListener("click", () => {
+        let titulo = "Empreendimentos de Apoio ao Turismo";
+        generateCardsFromIndicador(indicadores[4], titulo);
+    });
+    
+    empreenEntreteniCard.addEventListener("click", () => {
+        let titulo = "Empreendimentos de Entretenimento";
+        generateCardsFromIndicador(indicadores[5], titulo);
+    });
+    
+    guiaMEICard.addEventListener("click", () => {
+        let titulo = "Guias Turísticos MEI";
+        generateCardsFromIndicador(indicadores[6], titulo);
+    });
+    
+    guiaPFCard.addEventListener("click", () => {
+        let titulo = "Guias Turísticos Pessoa Física";
+        generateCardsFromIndicador(indicadores[7], titulo);
+    });
+    
+    locadoraVeiculoCard.addEventListener("click", () => {
+        let titulo = "Locadoras de Veículos";
+        generateCardsFromIndicador(indicadores[8], titulo);
+    });
+    
+    meioHospCard.addEventListener("click", () => {
+        let titulo = "Meios de Hospedagem";
+        generateCardsFromIndicador(indicadores[9], titulo);
+    });
+    
+    organiEventCard.addEventListener("click", () => {
+        let titulo = "Organizadoras de Eventos";
+        generateCardsFromIndicador(indicadores[10], titulo);
+    });
+    
+    parqueTemaCard.addEventListener("click", () => {
+        let titulo = "Parques Temáticos";
+        generateCardsFromIndicador(indicadores[11], titulo);
+    });
+    
+    prestEspCard.addEventListener("click", () => {
+        let titulo = "Prestadores de Serviços Especializados";
+        generateCardsFromIndicador(indicadores[12], titulo);
+    });
+    
+    prestServInfraCard.addEventListener("click", () => {
+        let titulo = "Prestadores de Serviços de Infraestrutura";
+        generateCardsFromIndicador(indicadores[13], titulo);
+    });
+    
+    restBarCafeCard.addEventListener("click", () => {
+        let titulo = "Restaurantes, Bares & Afins";
+        generateCardsFromIndicador(indicadores[14], titulo);
+    });
+    
+    transpTurCard.addEventListener("click", () => {
+        let titulo = "Transportadoras Turísticas";
+        generateCardsFromIndicador(indicadores[15], titulo);
+    });
+    
+    
+
+
+    function toggleSelectorAndIndicadores () {
+        const selectorDiv = document.querySelector(".selector");
+        const indicadoresSection = document.querySelector(".indicadores");
+
+        selectorDiv.classList.toggle("opacity-zero");
+        indicadoresSection.classList.toggle("opacity-zero");
+       // elemH1.classList.toggle("opacity-zero")
+
+        setTimeout(() => {
+            selectorDiv.classList.toggle("display-none");
+            indicadoresSection.classList.toggle("display-none");
+        //    elemH1.classList.toggle("display-none");
+        },1000);
+    }
+    
 }
        
-
 
 
 
